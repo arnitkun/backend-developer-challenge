9022c533d87696edacf7819b072f64739bebe5d5
arnitkun <ankit.nitagartala@gmail.com>
2020-03-12 22:59:55 +0530

    initial parsing added

--- utils/parse.js
+++ utils/parse.js
@@ -1,5 +1,30 @@
+const csvParser = require('csv-parser');
+const fs = require('fs')
+
+
+
 function parseCSV(csvFile, currency) {
+    let importedData = [];
     console.log(currency);
+    fs.createReadStream(csvFile)
+        .on('error', (error) => {
+            console.log(error);
+        })
+        .pipe(csvParser())
+        .on('data', (row) => {
+            importedData.push(row);
+        })
+        .on('end', ()=>{
+            console.log('CSV processed');
+            disburse(importedData, currency);
+        })
+}
+
+function disburse(data, currency) {
+    console.log(currency);
+    data.forEach(element => {
+        console.log(element);
+    });
 }
 
 module.exports = {
