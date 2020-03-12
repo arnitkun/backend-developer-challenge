a8d8d9ff7bc828f39c8882fb57f095f45c2c62bd
arnitkun <ankit.nitagartala@gmail.com>
2020-03-12 22:26:55 +0530

    added route for handling the submitted data

---
+++ routes/csvhandler.js
@@ -0,0 +1,66 @@
+const multer = require('multer');
+const path = require('path');
+const {parseCSV} = require('../utils/parse');
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+    destination: './public/uploads/',
+    filename: function(req, file, cb){
+      cb(null,file.fieldname + path.extname(file.originalname));
+    }
+  });
+  
+  // Init Upload
+  const upload = multer({
+    storage: storage,
+    limits:{fileSize: 1000000},
+    fileFilter: function(req, file, cb){
+      checkFileType(file, cb);
+    }
+  }).single('csvfile');
+  
+  // Check File Type
+  function checkFileType(file, cb){
+    // Allowed ext
+    const filetypes = /csv|CSV/;
+    // Check ext
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    // Check mime
+  
+    if(extname){
+      return cb(null,true);
+    } else {
+      cb('Error: Images Only!');
+    }
+  }
+  
+
+
+function csvHandler(req, res, next) {
+ 
+      upload(req, res, (err) => {
+        if(err){
+          res.render('index', {
+            msg: err
+          });
+        } else {
+          if(req.file == undefined){
+            res.render('index', {
+              msg: 'Error: No File Selected!'
+            });
+          } else {
+            console.log(req.body.currency);
+            //parse the file here
+            parseCSV(req.file, req.body.currency);
+            res.render('index', {
+              msg: 'File Uploaded!'
+            });
+          }
+        }
+      });
+
+}
+
+module.exports = {
+    csvHandler
+}
