fe41292340bf569e5434a1d924aab311cd35a91b
arnitkun <ankit.nitagartala@gmail.com>
2020-03-13 15:14:15 +0530

    added CSV writing functionality

--- utils/parse.js
+++ utils/parse.js
@@ -3,12 +3,16 @@ const fs = require('fs')
 const fetch = require('node-fetch');
 var URL = 'https://api.exchangeratesapi.io/latest';
 const {calculate} = require('./converter');
+const {writeCsv} = require('./writeCsv');
 
 
-
+/**
+ * 
+ * @param {string} csvFile Path to the csv file
+ * @param {string} currency currency with respect to which conversion is to be done 
+ */
 function parseCSV(csvFile, currency) {
     let importedData = [];
-    // console.log(currency);
     fs.createReadStream(csvFile)
         .on('error', (error) => {
             console.log(error);
@@ -21,20 +25,24 @@ function parseCSV(csvFile, currency) {
             console.log('CSV processed');
             let list = {};
             let data = removeEmptyEntries(importedData, currency);
-            calculate(data, URL, 'INR')
+            calculate(data, URL, currency)
             .then(data => {
                 let ans = aggregate(data.supported);
-                console.log(ans)
+                console.log(ans);
+                writeCsv(ans);
             })
         })
 }
 
+/**
+ * Helper function to remove extra empty columns from the extracted array.
+ * @param {List} data data extracted from csv file 
+ * @param {string} currency currency with respect to which conversion is to be done
+ */
 function removeEmptyEntries(data, currency) {
     let collectedData = []
-    console.log(currency);
     data.forEach(element => {
         if(element['Order Id'] == '') {
-            //do nothing
         } else {
             collectedData.push(element);
         }
@@ -43,6 +51,10 @@ function removeEmptyEntries(data, currency) {
     return collectedData;
 }
 
+/**
+ *  Aggregates donations on basis of common donators.
+ * @param {List} list List of donation objects 
+ */
 function aggregate(list) {
     const res = list.reduce((acc, d) => {
         let found = acc.find( a => a.name === d.Nonprofit);
@@ -63,7 +75,6 @@ function aggregate(list) {
               acc.forEach(ob=> {
                   if(ob.name === found.name){
                       ob.donation += 1
-                      
                       ob["Fee"] += fee
                       ob.Donation_amount += donation_amount
                   }
@@ -71,7 +82,6 @@ function aggregate(list) {
             }
             return acc;
           }, []);
-
     return res;
 }
 
