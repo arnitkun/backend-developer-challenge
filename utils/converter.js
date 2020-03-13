e329bec9d6905d157fa2afcc0bad8b62f5438248
arnitkun <ankit.nitagartala@gmail.com>
2020-03-13 15:14:00 +0530

    updated converted.js

--- utils/converter.js
+++ utils/converter.js
@@ -1,6 +1,5 @@
 const fetch = require('node-fetch');
 
-
 /**
  * 
  * @param {string} url URL of the api that fetches the current exchange rates 
@@ -20,32 +19,25 @@ async function getRates(url, currency) {
     }
 }
 
-    async function calculate(data, url, currency) {
-        let supported = [];
-        let unsupported = [];
-        let filtered = {};
-        let rates = await getRates(url, currency);
-        console.log(rates);
-        console.log("The target:" + rates[currency]);
-        // console.log(typeof jsonData)
-        data.forEach(element=> {
-            // element = JSON.parse(ele);
-            if(Object.keys(rates).includes(element["Donation Currency"]) && Object.keys(rates).includes(currency)) {
-                let converted = {}
-                converted["Donation Amount"] = parseFloat(element["Donation Amount"]) * rates[currency];
-                converted["Fee"] = parseFloat(element.Fee) * rates[currency];
-                // converted["Date"] = element["Date"];
-                // console.log(converted["Date"]);
-                converted.Nonprofit = element.Nonprofit;
-                converted["Order Id"] = element["Order Id"];
-                converted.currency = currency;
-                supported.push(converted);
-
-            } else {
-                console.log("Unknown currency: " + element["Donation Currency"]);
-                unsupported.push(element);
-            }
-        })
+async function calculate(data, url, currency) {
+    let supported = [];
+    let unsupported = [];
+    let filtered = {};
+    let rates = await getRates(url, currency);
+    
+     data.forEach(element=> {
+        if(Object.keys(rates).includes(element["Donation Currency"]) && Object.keys(rates).includes(currency)) {
+            let converted = {}
+            converted["Donation Amount"] = parseFloat(element["Donation Amount"]) * rates[currency];
+            converted["Fee"] = parseFloat(element.Fee) * rates[currency];
+            converted.Nonprofit = element.Nonprofit;
+            converted["Order Id"] = element["Order Id"];
+            converted.currency = currency;
+            supported.push(converted);
+        } else {
+            unsupported.push(element);
+        }
+    })
 
         filtered.supported = supported;//contains the list of donations whose currency is supported by exchangeratesapi
         filtered.unsupported = unsupported;//contains the list of donations whose currency is NOT supported by exchangeratesapi
@@ -57,5 +49,3 @@ async function getRates(url, currency) {
     module.exports = {
         calculate
     }
-
-    
