aa50ac5444f08a14a5bf835e0773d4b43744919e
arnitkun <ankit.nitagartala@gmail.com>
2020-03-13 14:33:22 +0530

    updated parse.js

--- utils/parse.js
+++ utils/parse.js
@@ -1,11 +1,14 @@
 const csvParser = require('csv-parser');
 const fs = require('fs')
+const fetch = require('node-fetch');
+var URL = 'https://api.exchangeratesapi.io/latest';
+const {calculate} = require('./converter');
 
 
 
 function parseCSV(csvFile, currency) {
     let importedData = [];
-    console.log(currency);
+    // console.log(currency);
     fs.createReadStream(csvFile)
         .on('error', (error) => {
             console.log(error);
@@ -16,16 +19,63 @@ function parseCSV(csvFile, currency) {
         })
         .on('end', ()=>{
             console.log('CSV processed');
-            disburse(importedData, currency);
+            let list = {};
+            let data = removeEmptyEntries(importedData, currency);
+            calculate(data, URL, 'INR')
+            .then(data => {
+                let ans = aggregate(data.supported);
+                console.log(ans)
+            })
         })
 }
 
-function disburse(data, currency) {
+function removeEmptyEntries(data, currency) {
+    let collectedData = []
     console.log(currency);
     data.forEach(element => {
-        console.log(element);
+        if(element['Order Id'] == '') {
+            //do nothing
+        } else {
+            collectedData.push(element);
+        }
     });
+    console.log(collectedData.length);
+    return collectedData;
 }
+
+function aggregate(list) {
+    const res = list.reduce((acc, d) => {
+        let found = acc.find( a => a.name === d.Nonprofit);
+        let donation_amount = d["Donation Amount"]
+        let fee = d["Fee"];
+        let currency = d.currency;
+
+        if(!found) {
+            acc.push({
+                name: d.Nonprofit,
+                donation: 1, 
+                Fee: d["Fee"],
+                currency: d.currency,
+                Donation_amount: donation_amount
+            })
+          } else {
+              
+              acc.forEach(ob=> {
+                  if(ob.name === found.name){
+                      ob.donation += 1
+                      
+                      ob["Fee"] += fee
+                      ob.Donation_amount += donation_amount
+                  }
+              })
+            }
+            return acc;
+          }, []);
+
+    return res;
+}
+
+
 
 module.exports = {
     parseCSV
